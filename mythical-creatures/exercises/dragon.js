1d0fe8e43654590104412f552ac8df159f0432af
Whitney Wilkes <wilkes.whit@gmail.com>
2021-09-13 14:47:14 -0400

    Complete Dragon exercise

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.counter = 0;
+};
 
+greet() {
+    return `Hi, ${this.rider}!`
+};
+
+eat() {
+    this.counter++;
+    if (this.counter === 3) {
+        this.hungry = false;
+        };
+    };
+};
+
+
+
+
+module.exports = Dragon;
